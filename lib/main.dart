9db70c994a7779b9af1b4ad11abb95c6526ba425
Akmaljon Abdirakhimov <AkmaljonAbdirakhimov@gmail.com>
2024-06-05 22:59:06 +0500

    course app

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,56 @@
+import 'package:dars51/utils/routes.dart';
+import 'package:dars51/views/screens/courses/add_course_screen.dart';
 import 'package:dars51/views/screens/home_screen.dart';
-import 'package:dars51/views/screens/second_screen.dart';
+import 'package:dars51/views/screens/main_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const CourseApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class CourseApp extends StatelessWidget {
+  const CourseApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorSchemeSeed: Colors.purple,
-      ),
-      initialRoute: '/',
-      // routes: {
-      //   '/': (context) => const HomeScreen(),
-      //   '/ikkinchi-sahifa': (context) => const SecondScreen(),
-      // },
-      onGenerateRoute: (settings) {
-        print(settings.name);
-        if (settings.name == "/ikkinchi-sahifa") {
-          return MaterialPageRoute(builder: (ctx) {
-            return SecondScreen(username: settings.arguments.toString());
-          });
-        }
-
-        return MaterialPageRoute(builder: (ctx) {
-          return const HomeScreen();
-        });
+      initialRoute: RouteNames.mainScreen,
+      routes: {
+        RouteNames.mainScreen: (ctx) => const MainScreen(),
+        RouteNames.addCourseScreen: (ctx) => const AddCourseScreen(),
       },
     );
   }
 }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       theme: ThemeData(
+//         colorSchemeSeed: Colors.purple,
+//       ),
+//       initialRoute: '/',
+//       // routes: {
+//       //   '/': (context) => const HomeScreen(),
+//       //   '/ikkinchi-sahifa': (context) => const SecondScreen(),
+//       // },
+//       onGenerateRoute: (settings) {
+//         print(settings.name);
+//         if (settings.name == "/ikkinchi-sahifa") {
+//           return MaterialPageRoute(builder: (ctx) {
+//             return SecondScreen(username: settings.arguments.toString());
+//           });
+//         }
+
+//         return MaterialPageRoute(builder: (ctx) {
+//           return const HomeScreen();
+//         });
+//       },
+//     );
+//   }
+// }
